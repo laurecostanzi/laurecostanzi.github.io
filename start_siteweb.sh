38b0c5cd9ab90d50b6cb119b76ad50b7ce32d5fe
Laure Kopacin <laure@chateau.home>
2020-03-13 17:28:03 -0700

    cleanup

--- start_siteweb.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IP=$(ip addr show wlan0 | grep "noprefixroute wlan0" | awk '{print $2}' | cut -d '/' -f 1)
-echo "Site adresse: http://${IP}:8000"
-cd ~/Siteweb/startbootstrap-creative-master/
-python -m SimpleHTTPServer
-sleep 10
